e4d83a316d5e3a7f40c374ce9b8b5b759fd56650
John Schultz <jschultz@salesforce.com>
2017-10-06 08:18:54 -0600

    Adding more comments for clarity

--- src/aura/GoToContentNote/GoToContentNoteController.js
+++ src/aura/GoToContentNote/GoToContentNoteController.js
@@ -1,10 +1,15 @@
 ({
-	doNoteCreation : function(component, event, helper) {
-		var action = component.get('c.createNote');
+    // this lightning controller method is called when the button is clicked.
+    doNoteCreation : function(component, event, helper) {
+        // createNote is the apex controller method that will be called.
+        var action = component.get('c.createNote');
         
         action.setCallback(this, function(response){
+            // after the apex controller method returns a ContentNote record id,
+            // we navigate to it using the standard lightning event force:navigateToSObject.
             var newNoteEvent = $A.get('e.force:navigateToSObject');
             
+            // here we're supplying the event with the record id of the newly created note.
             newNoteEvent.setParams({
                 recordId : response.getReturnValue()
             });
@@ -13,5 +18,5 @@
         });
         
         $A.enqueueAction(action);
-	},
+    },
 })
